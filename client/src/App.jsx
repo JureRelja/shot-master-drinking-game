e6cb5a14636954bfaa5259b7f0ba18a8cb06cbd3
Ilija Mihajlovic <iilija879a@gmail.com>
2022-12-08 21:47:32 +0100

    main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "../components/navbar/Navbar";
+import Main from "../components/navbar/Navbar";
 
 export default function App() {
   return (
     <>
-      <Navbar />
+      <Main />
     </>
   );
 }
