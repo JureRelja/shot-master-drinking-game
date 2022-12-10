84a6a935fc8908806355cc584c8c6a33f0f400d8
JureRelja <jurereljanovic99@gmail.com>
2022-12-10 10:24:10 +0100

    nesto

--- client/components/main/Main.jsx
+++ client/components/main/Main.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Main = () => {
-  return (
-    <div className='text-3xl font-bold underline'>Main</div>
-  )
+    let BAC = 0;
+    let kile = 0;
+    const [counter, setCounter] = useState(0);
+    
+    
+    //increase counter
+    const shootEvent = () => {
+        setCounter(count => count + 1);
+    };
+
+    const kileEvent = event => {
+       kile = event.target.value
+    }
+
+    return (
+        <>
+        <div className='text-3xl font-bold underline'>
+            <span className='text-3xl font-bold'>Broj shotova: {counter}</span>
+            <input type="number" onChange={kileEvent}/>
+            <button onClick={shootEvent}>Shootiraj</button>
+        </div>
+        <div className='text-3xl font-bold underline'>Main</div>
+        </>
+        
+    );
+
 }
 
 export default Main
