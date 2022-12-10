36d41f6237621b5e4700910eb8ffb55872774562
JureRelja <jurereljanovic99@gmail.com>
2022-12-10 14:56:45 +0100

    df

--- client/components/main/Main.jsx
+++ client/components/main/Main.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@material-tailwind/react";
 
 const Main = () => {
-    let r = 0.68;
     const g_alch = 10.428;
 
-    const [counter, setCounter] = useState(0);
+    const [brojPica, setBrojPica] = useState(1);
     const [kile, setKile] = useState(0);
-    const [gender, setGender] = useState("");
     const [buttonAndBacStyle, setButtonAndBacStyle] = useState("hidden")
     const [formStyle, setFormStyle] = useState("block")
     const [BAC, setBAC] = useState(0)
+    const [r, setR] = useState(0)
     
     const kileEvent = event => {
        setKile(event.target.value * 1000)
     }
 
     const genderEvent = event => {
-        setGender(event.target.value)
-        if (gender == "Muško") {
-            r = 0.68
+        console.log(event.target.value)
+        //console.log(gender)
+        if (event.target.value == "Žensko") {
+            setR(0.55)
+        }
+        else if (event.target.value == "Muško") {
+            setR(0.68)
         }
     }
     
@@ -31,8 +34,8 @@ const Main = () => {
     }
 
     const shootEvent = () => {
-        setCounter(count => count + 1);
-        setBAC((counter*g_alch/(kile*r))*1000)
+        setBrojPica(count => count + 1);
+        setBAC((brojPica*g_alch/(kile*r))*1000)
     };
     return (
         <>
@@ -40,13 +43,14 @@ const Main = () => {
             <form onSubmit={submitEvent} className={`${formStyle}`}>
                 <input className='bg-green-500' type="number" onChange={kileEvent}/>
                 <select onChange={genderEvent}>
+                    <option value="" selected disabled hidden>Odaberi spol</option>
                     <option value="Muško">Muško</option>
                     <option value="Žensko">Žensko</option>
                 </select>
                 <button type="submit">Submit</button>
             </form>
             
-            <Button variant="outlined" color="blue" className={`${buttonAndBacStyle}`} onClick={shootEvent}>Šotiraj</Button>
+            <Button variant="outlined" color="green" className={`${buttonAndBacStyle}`} onClick={shootEvent}>Šotiraj</Button>
             <span className={`${buttonAndBacStyle}`}>{BAC}</span>
         </div>
         </>
