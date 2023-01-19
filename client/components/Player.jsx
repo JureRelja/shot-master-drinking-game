bbddfe92e560068e19560f80e7c3f7b292d09e99
Ilija Mihajlovic <iilija879a@gmail.com>
2023-01-19 07:34:29 +0100

    test layout

--- client/components/Player.jsx
+++ client/components/Player.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const Player = ({ igraci, Player1, Player2 }) => {
   return (
     <>
-      <div className="flex justify-evenly">
+      <div className={`grid grid-cols-${igraci.length}`}>
         {igraci.map((igrac) =>
           igraci.indexOf(igrac) == 0 ? (
             <div key={igrac.socketID}>
               <span>Username: {igrac.userName}</span>
-              <Player1 />
+              <Player1 className="h-[200px] w-[200px]" />
             </div>
           ) : (
             <div key={igrac.socketID}>
               <span>Username: {igrac.userName}</span>
-              <Player2 />
+              <Player2 className="h-[200px] w-[200px]" />
             </div>
           )
         )}
