27ff0b0780d4b92d554c3883310dfa6f71ce668b
ilija.mihajlovic <iilija879a@gmail.com>
2023-01-23 16:19:17 +0100

    rewriting gamepage so that it's scalable

--- client/components/Player.jsx
+++ client/components/Player.jsx
@@ -2,23 +2,21 @@ import React from "react";
 
 const Player = ({ igraci, Player1, Player2 }) => {
   return (
-    <>
-      <div className={`grid grid-cols-${igraci.length}`}>
-        {igraci.map((igrac) =>
-          igraci.indexOf(igrac) == 0 ? (
-            <div key={igrac.socketID}>
-              <span>Username: {igrac.userName}</span>
-              <Player1 className="h-[200px] w-[200px]" />
-            </div>
-          ) : (
-            <div key={igrac.socketID}>
-              <span>Username: {igrac.userName}</span>
-              <Player2 className="h-[200px] w-[200px]" />
-            </div>
-          )
-        )}
-      </div>
-    </>
+    <div className={`grid grid-cols-${igraci.length}`}>
+      {igraci.map((igrac) =>
+        igraci.indexOf(igrac) == 0 ? (
+          <div key={igrac.socketID}>
+            <span>Username: {igrac.userName}</span>
+            <Player1 className="h-[200px] w-[200px]" />
+          </div>
+        ) : (
+          <div key={igrac.socketID}>
+            <span>Username: {igrac.userName}</span>
+            <Player2 className="h-[200px] w-[200px]" />
+          </div>
+        )
+      )}
+    </div>
   );
 };
 
