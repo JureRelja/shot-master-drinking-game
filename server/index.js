dd95b6f30cd2748e867aa16a56b59ec51ee7e8e6
JureRelja <jurereljanovic99@gmail.com>
2023-02-02 20:56:41 +0100

    port fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 const http = require("http").Server(app);
 const cors = require("cors");
 const e = require("express");
